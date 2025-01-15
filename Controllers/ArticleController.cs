89ea25555899bd087837c2a4df90b94a18d1b082
LHT082024 <LHT082024@gmail.com>
2025-01-15 13:20:39 +0100

    added comments

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 
 namespace Newspaper.Controllers
 {
+    // i begynnelsen bare lag basic crud requests.
     public class ArticleController
     {
-        
+
     }
 }
