12c79c216e82c0221b9b492ebb17cb12217528eb
LHT082024 <LHT082024@gmail.com>
2025-01-16 11:40:17 +0100

    shit working

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -2,45 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Newspaper.Models;
+
 
 namespace Newspaper.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProfileController : Controller
+    public class ProfileController : ControllerBase
     {
-        List<ProfileModel> profiles = new List<ProfileModel>()
+        private static List<ProfileModel> profiles = new List<ProfileModel>()
         {
             new ProfileModel {Name = "Testing", Id = 1, Password = "lol2", ProfileType = "Editor"}
 
         };
 
 
-        [HttpGet]
-        public IActionResult GetAllProfiles()
-        {
-            return Ok(profiles);
-        }
+
         [HttpPost]
-        public IActionResult CreateProfiles([FromBody] ProfileModel _profileModel)
+        public IActionResult Post([FromBody] ProfileModel _profileModel)
         {
 
-            if (profiles == null)
+            if (_profileModel == null)
             {
                 return BadRequest("something went wrong");
             }
+
             _profileModel.Id = profiles.Count + 1;
             profiles.Add(_profileModel);
-            return CreatedAtAction(nameof(GetAllProfiles), new
-            {
-                name = _profileModel.Name,
-                id = _profileModel.Id,
-                password = _profileModel.Password,
-                _profileModel.ProfileType
-            }, _profileModel);
+
+            return CreatedAtAction(nameof(Get), new { id = _profileModel.Id }, profiles);
+        }
+
+
+        [HttpGet]
+        public IEnumerable<ProfileModel> Get()
+        {
+            return profiles;
         }
 
     }
