c91e74732278cdabd2736e054f7570e7df599da9
LHT082024 <LHT082024@gmail.com>
2025-01-16 11:24:46 +0100

    coding hates me today

--- Models/ArticleModel.cs
+++ Models/ArticleModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Newspaper.Models
 {
     public class ArticleModel
     {
-        
+        public int Tier { get; set; }
+        public string? Headline { get; set; }
+        public string? Story { get; set; }
+        public int ID { get; set; }
+
     }
 }
