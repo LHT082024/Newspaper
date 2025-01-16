9a480ec7c3ed72be3a7119ee92790a86204e67bf
LHT082024 <LHT082024@gmail.com>
2025-01-16 13:30:33 +0100

    added migrations

---
+++ Migrations/20250116123022_InitialMigration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Newspaper.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "articleModels",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tier = table.Column<int>(type: "int", nullable: false),
+                    Headline = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Story = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_articleModels", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "profileModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProfileType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_profileModels", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "articleModels");
+
+            migrationBuilder.DropTable(
+                name: "profileModels");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using Newspaper.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DbContextClass>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 builder.Services.AddControllers();
