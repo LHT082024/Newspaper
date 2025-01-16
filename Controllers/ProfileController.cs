e1f1c25a8f34c07fe2383567fff768d2a40a3256
LHT082024 <LHT082024@gmail.com>
2025-01-16 09:44:38 +0100

    profiles

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Newspaper.Models;
 
 namespace Newspaper.Controllers
 {
-    public class ProfileController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfileController : ControllerBase
     {
-        
+        List<ProfileModel> profiles = new List<ProfileModel>()
+        {
+            new ProfileModel {Name = "Testing", Id = 1, Password = "lol2", ProfileType = "Editor"}
+
+        };
+
+        [HttpGet("List of profiles")]
+        public async Task<IActionResult> GetAllProifles()
+        {
+            return Ok(profiles);
+        }
+
     }
 }
