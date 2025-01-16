ea0fd2cf3a4c805f8d50a31053effcfe5575704a
LHT082024 <LHT082024@gmail.com>
2025-01-16 11:18:34 +0100

    trying againgg

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Newspaper.Controllers
 {
     // i begynnelsen bare lag basic crud requests.
-    public class ArticleController
+    public class ArticleController : ControllerBase
     {
 
     }
