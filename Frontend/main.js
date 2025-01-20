4dcad8bd12d2859e24ca22894cd3389f207f2a94
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-20 14:19:34 +0100

    removed extra code

--- Frontend/main.js
+++ Frontend/main.js
@@ -39,20 +39,3 @@ async function createArticle() {
 }
 
 createArticle();
-
-/*async function ConnectBackend(url) {
-  try {
-    let response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('HTTP error! Status: ${response.status}');
-    }
-    let data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
-
-ConnectBackend('http://localhost:5095/api/Article').then((data) => {
-  console.log('Received from backend:', data);
-});*/
