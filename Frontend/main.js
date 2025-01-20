5867e7d6a612164b98dfc52530149c4634ae4eec
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-20 14:09:34 +0100

    fetch data with javscript

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,4 +1,19 @@
-async function ConnectBackend(url) {
+//fetch data with javascript
+async function fetchArticles() {
+  try {
+    let response = await fetch('http://localhost:5095/api/Article');
+    if (!response.ok) throw new Error('HTTP error ' + response.status);
+
+    let data = await response.json();
+    console.log('Articles:', data);
+  } catch (error) {
+    console.error('Fetch Error:', error);
+  }
+}
+
+fetchArticles();
+
+/*async function ConnectBackend(url) {
   try {
     let response = await fetch(url);
     if (!response.ok) {
@@ -13,4 +28,4 @@ async function ConnectBackend(url) {
 
 ConnectBackend('http://localhost:5095/api/Article').then((data) => {
   console.log('Received from backend:', data);
-});
+});*/
