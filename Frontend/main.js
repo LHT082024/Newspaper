baee293c974ab1c77f89b40832a5ee38df64d268
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-23 14:29:27 +0100

    remade the hammenu with help from youtube

--- Frontend/main.js
+++ Frontend/main.js
@@ -43,11 +43,11 @@ createArticle();
 //login
 async function Login() {}
 
-function myFunction() {
-  var x = document.getElementById('navigation');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-  }
-}
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+});
