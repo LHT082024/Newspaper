347dd6216243c865c03708fc31ac601262dacf15
LHT082024 <LHT082024@gmail.com>
2025-02-06 11:54:27 +0100

    trying stuff

---
+++ Frontend/searchbar.js
@@ -0,0 +1,7 @@
+//trying to make searchbar work
+function searchArticles() {
+    const searchInput = document.getElementById("search-input");
+    const articlesContainer = document.querySelector(".article-container");
+
+    
+}
